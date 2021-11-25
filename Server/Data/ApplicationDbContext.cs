50da269969633267bdbe42c6119761fab889f98f
JDzhambazov <jamby@mail.bg>
2021-11-25 18:15:52 +0200

    Add project files.

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace ElPro.Server.Data
+{
+    using ElPro.Server.Models;
+    using IdentityServer4.EntityFramework.Options;
+    using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Options;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
